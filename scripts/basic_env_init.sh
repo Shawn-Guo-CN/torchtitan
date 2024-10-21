95082c241b664e0045170cec2598163edbcf5aca
Shangmin Guo <shawnguo.cn@gmail.com>
2024-10-21 23:04:01 +0100

    add bash for basic env setup

---
+++ scripts/basic_env_init.sh
@@ -0,0 +1,17 @@
+set -e
+
+# NOTE: remember to update the HOME environment variable
+export HOME=/work/ec280/ec280/shawn
+export HF_HOME=/work/ec280/ec280/shawn/hf_home
+
+# load cuda module
+module load nvidia/nvhpc/24.5
+module load nvidia/cudnn/9.2.0-cuda-12.4
+echo ${NVHPC_ROOT}
+export CUDA_HOME=${NVHPC_ROOT}/cuda # or export CUDA_HOME=${NVHPC_ROOT}
+
+# activate vitural environment
+source /work/ec280/ec280/shawn/vtorchtitan/bin/activate
+
+# change to the working directory
+cd /work/ec280/ec280/shawn/torchtitan
